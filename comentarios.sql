4f8586e5ad3c8910b9d707dac054a8d69dade895
Esteban0010 <estebanpilchuman02@gmail.com>
2024-10-15 19:30:40 -0300

    ultimo sql

--- comentarios.sql
+++ comentarios.sql
@@ -1,18 +1,19 @@
 CREATE TABLE `evaluacion` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,  -- Identificador único para cada evaluación
+     `id_comentario` INT(11) NOT NULL,                         -- Clave foránea que relaciona el comentario con una evaluación
     `sentimiento` FLOAT NOT NULL,          -- Almacena el puntaje de sentimiento (análisis de sentimiento)
     `entidades` TEXT NOT NULL,             -- Almacena las entidades detectadas (formato JSON)
     `syntaxis` TEXT NOT NULL,              -- Almacena la estructura de la sintaxis (formato JSON)
-    PRIMARY KEY (`id`)                     -- Clave primaria en el campo `id`
+    PRIMARY KEY (`id`),                     -- Clave primaria en el campo `id`
+    FOREIGN KEY (`id_comentario`) REFERENCES `comentario`(`id`) ON DELETE CASCADE  -- Relación con evaluacion
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
 CREATE TABLE `comentarios` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,                     -- Identificador único del comentario
-    `id_evaluacion` INT(11) NOT NULL,                         -- Clave foránea que relaciona el comentario con una evaluación
     `autor` VARCHAR(255) NOT NULL,                            -- Nombre del autor del comentario
     `comentario` TEXT NOT NULL,                               -- El contenido del comentario
     `fecha_creacion` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,     -- Fecha de creación del comentario
     `pais` VARCHAR(16) NOT NULL,                              -- Pais del Comentario
     PRIMARY KEY (`id`),
-    FOREIGN KEY (`id_evaluacion`) REFERENCES `evaluacion`(`id`) ON DELETE CASCADE  -- Relación con evaluacion
+
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
