8de5840e8ccedc0e656b83be23fa0dee16504777
Esteban0010 <estebanpilchuman02@gmail.com>
2024-10-15 19:35:46 -0300

    ultimo sqaal

--- comentarios.sql
+++ comentarios.sql
@@ -1,3 +1,12 @@
+CREATE TABLE `comentarios` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,                     -- Identificador único del comentario
+    `autor` VARCHAR(255) NOT NULL,                            -- Nombre del autor del comentario
+    `comentario` TEXT NOT NULL,                               -- El contenido del comentario
+    `fecha_creacion` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,     -- Fecha de creación del comentario
+    `pais` VARCHAR(16) NOT NULL,                              -- País del comentario
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
 CREATE TABLE `evaluacion` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,  -- Identificador único para cada evaluación
     `id_comentario` INT(11) NOT NULL,      -- Clave foránea que relaciona el comentario con una evaluación
@@ -5,14 +14,5 @@ CREATE TABLE `evaluacion` (
     `entidades` TEXT NOT NULL,             -- Almacena las entidades detectadas (formato JSON)
     `syntaxis` TEXT NOT NULL,              -- Almacena la estructura de la sintaxis (formato JSON)
     PRIMARY KEY (`id`),                    -- Clave primaria en el campo `id`
-    FOREIGN KEY (`id_comentario`) REFERENCES `comentarios`(`id`) ON DELETE CASCADE  -- Relación con evaluacion
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
-
-CREATE TABLE `comentarios` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,                     -- Identificador único del comentario
-    `autor` VARCHAR(255) NOT NULL,                            -- Nombre del autor del comentario
-    `comentario` TEXT NOT NULL,                               -- El contenido del comentario
-    `fecha_creacion` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,     -- Fecha de creación del comentario
-    `pais` VARCHAR(16) NOT NULL,                              -- País del comentario
-    PRIMARY KEY (`id`)                                         -- Se elimina la coma extra aquí
+    FOREIGN KEY (`id_comentario`) REFERENCES `comentarios`(`id`) ON DELETE CASCADE  -- Relación con comentario
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
