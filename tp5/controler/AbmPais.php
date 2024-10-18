5a334fe240c482266d563541e812c7a6d5420cbd
Francisco Pandolfi Jimenez <francisco.pandolfi@est.fi.uncoma.edu.ar>
2024-10-18 03:31:30 -0300

    Agregue el AbmPais al actionPais.php para manipular un array de datos de la libreria

--- tp5/controler/AbmPais.php
+++ tp5/controler/AbmPais.php
@@ -272,4 +272,30 @@ class AbmPais
 
         return null; 
     }
+
+    public function paisInformacion($param){
+        $pais = country($param);
+    
+        $array = [
+            "nombre" => $pais->getName(),  
+            "nombreOficial" => $pais->getOfficialName(), 
+            "capital" => $pais->getCapital(),
+            "gentilicio" => $pais->getDemonym(),
+            "idioma" => $pais ->getLanguages(),
+            "codigoPostal" => $pais ->usesPostalCode(),
+            "numeroIso" => $pais->getIsoNumeric(),
+            "continente" => $pais->getContinent(),
+            "limitrofes" => $pais->getBorders(),
+            "maxLatitud" => $pais->getMaxLatitude(),
+            "minLatitud" => $pais->getMinLatitude(), 
+            "area" => $pais->getArea(),
+            "region" => $pais->getRegion(),
+            "sinLitoral" => $pais->isLandlocked(),
+            "emoji" => $pais->getEmoji(),
+            "bandera" => $pais->getFlag()
+            
+        ];
+        
+        return $array;
+    }
 }
--- tp5/vista/action/actionPais.php
+++ tp5/vista/action/actionPais.php
@@ -3,18 +3,15 @@ include_once '../../configuracion.php';
 
 $datos = data_submitted();
 
-// Validar si se envió el país antes de intentar usar la función country.
+// Validar si se envió el país antes de intentar usar la instancia de una clase AbmPais().
 if (isset($datos['codigoPais'])) {
-    $pais = country($datos['codigoPais']);
-    $nombrePais = $pais->getName();
-} else {
-    $nombrePais = '';
+    $country = new AbmPais();
+    $colPaisInfo = $country->paisInformacion($datos['codigoPais']); // Obtiene un array de informacion del pais
 }
 
-
-
+// creacion de instancias de clase AbmComentario y AbmEvaluacion
 $objComentario = new AbmComentario();
-$arrayComentarios = $objComentario->darArray(['pais' => $nombrePais]); // Obtener el array de comentarios
+$arrayComentarios = $objComentario->darArray(['pais' => $colPaisInfo['nombre']]); // Obtener el array de comentarios
 $objEvaluaciones = new AbmEvaluacion();
 $arrayEvaluaciones = $objEvaluaciones->darArray();
 
@@ -44,28 +41,79 @@ include_once "../estructura/Header.php";
         <!-- Card para mostrar la información del país -->
         <?php
 
-        if ($pais) {
+        if ($colPaisInfo) {
 
             echo "<div class='card shadow'>";
+
             echo "<div class='card-header bg-primary text-white text-center'>";
-            echo "<h2>" . $nombrePais . "</h2>";
+            echo "<h2>" . $colPaisInfo['nombre'] . "</h2>";
             echo "</div>";
 
             echo "<div class='card-body'>";
-            echo "<p><strong>Nombre: </strong>" . $pais->getName() . "</p>";
-            echo "<p><strong>Capital: </strong>" . $pais->getCapital() . "</p>";
-            //echo "<p><strong>Moneda: </strong>" . $moneda . "</p>";
-
-            // idioma del país 
+            
+            // Nombre
+            echo "<p><strong>Nombre: </strong>" . $colPaisInfo['nombre'] . "</p>";
+            
+            // Capital
+            echo "<p><strong>Capital: </strong>" . $colPaisInfo['capital'] . "</p>";
+            
+            // Nombre Oficial
+            echo "<p><strong>Nombre Oficial: </strong>" . $colPaisInfo['nombreOficial'] . "</p>";
+            
+            // Gentilicio
+            echo "<p><strong>Gentilicio: </strong>" . $colPaisInfo['gentilicio'] . "</p>";
+            
+            // Idiomas
             echo "<p><strong>Idioma(s):</strong>";
-            foreach ($pais->getLanguages() as $idioma) {
-                echo " " . $idioma . " ";
+            foreach ($colPaisInfo['idioma'] as $idioma) {
+                echo " " . $idioma . "";
+            }
+
+            // Si Tiene Codigo Postal
+            echo "</p>";
+            if ($colPaisInfo['codigoPostal'] > 0) {
+                echo "<p><strong>Uso de Codigo Postal: </strong>Si";
+            } else {
+                echo "<p><strong>Uso de Codigo Postal: </strong>No";
+            }
+            
+            // Numero ISO
+            echo "<p><strong>Numero de ISO: </strong>" . $colPaisInfo['numeroIso'] . "</p>";
+            
+            // Continente
+            echo "<p><strong>Continente: </strong>" . $colPaisInfo['continente'] . "</p>";
+
+            // Limitrofe
+            echo "<p><strong>Limitrofe:</strong>";
+            foreach ($colPaisInfo['limitrofes'] as $limitePais) {
+                echo " " . $limitePais . "";
             }
             echo "</p>";
 
-            //echo "<p><strong>Paises limitrofes(s): </strong>" . $limitrofes . "</p>";
-            echo "<p><strong>Emoji: </strong>" . $pais->getEmoji() . "</p>";
-            echo "<div><strong>Bandera(s): </strong><div>" . $pais->getFlag() . "</div></div>";
+            // Latitud Maxima
+            echo "<p><strong>Latitud Maxima: </strong>" . $colPaisInfo['maxLatitud'] . "</p>";
+            
+            // Latitud Minima
+            echo "<p><strong>Latitud Minima: </strong>" . $colPaisInfo['minLatitud'] . "</p>";
+
+            // Area
+            echo "<p><strong>Area: </strong>" . $colPaisInfo['area'] . "</p>";
+
+            // Region
+            echo "<p><strong>Region: </strong>" . $colPaisInfo['region'] . "</p>";
+            
+            // Litoral
+            if ($colPaisInfo['sinLitoral']) {
+                echo "<p><strong>Litoral con el Mar: </strong>No";
+            } else {
+                echo "<p><strong>Litoral con el Mar: </strong>Si";
+            }
+
+            // Emoji
+            echo "<p><strong>Emoji: </strong>" . $colPaisInfo['emoji'] . "</p>";
+
+            // Bandera
+            echo "<div><strong>Bandera(s): </strong><div>" . $colPaisInfo['bandera'] . "</div></div>";
             echo "</div>";
             echo "</div>";
 
@@ -75,30 +123,35 @@ include_once "../estructura/Header.php";
 
             echo "<div class='card shadow'>";
             echo "<div class='card-header bg-success text-white text-center'>";
-            echo "<h3>Dejanos tu comentario si ya visitaste " . $pais->getName() . "</h3>";
+            echo "<h3>Dejanos tu comentario si ya visitaste " . $colPaisInfo['nombre'] . "</h3>";
             echo "</div>";
 
             echo "<div class='card-body'>";
 
+            // Formulario
             echo "<form id='comment-form' action='./actionEvaluacion.php' method='POST'>";
 
+            // Input Autor
             echo "<div class='form-group mb-3'>";
             echo "<label for='autor' class='form-label'>Autor:</label>";
-            echo "<input  ut type='text' class='form-control' name='autor' id='autor' required>";
+            echo "<input type='text' class='form-control' name='autor' id='autor' required>";
             echo "</div>";
 
+            // Input Mensaje
             echo "<div class='form-group mb-3'>";
             echo "<label for='msj' class='form-label'>Mensaje:</label>";
             echo "<input type='text' class='form-control' name='msj' id='msj' required>";
             echo "</div>";
 
             // Campo oculto para el país     
-            echo "<input type='hidden' id='pais' name='pais' value='" . $nombrePais . "'>";
-            // Campo oculto para el coidgo ISO del país  
+            echo "<input type='hidden' id='pais' name='pais' value='" . $colPaisInfo['nombre'] . "'>";
+            // Campo oculto para el codigo ISO del país  
             echo "<input type='hidden' id='codigo' name='codigo' value='" . $datos['codigoPais'] . "'>";
 
+            // Input Submit
             echo "<input type='submit' class='btn btn-primary w-100' value='Enviar'></input>";
 
+            // Boton Volver
             echo "<a href='../indexPrincipal.php' class='btn btn-secondary mt-2'>Volver Formulario</a>";
 
             echo "</form>";
