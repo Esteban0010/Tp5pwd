5a2ebd9d65ebffdbe9275835d9b7cb10ec735a16
Esteban Martin Pilchuman <estebanpilchuman02@gmail.com>
2024-10-18 00:45:38 -0300

    cambio d enombre  credenciales api

--- tp5/controler/AbmNaturalLanguage.php
+++ tp5/controler/AbmNaturalLanguage.php
@@ -9,7 +9,7 @@ class AbmNaturalLanguage {
     public function __construct()
     {
         // Configuración del cliente de Google Cloud Natural Language
-        $config = include('../../configuracionn.php');
+        $config = include('../../credencialesGoogle.php');
         $this->languageClient = new LanguageClient([
             'projectId' => $config['google_project_id'],
             'keyFilePath' => '../../../analisis-de-datos-437621-f4b9e9b5a2af.json' // Reemplaza con la ruta a tu archivo de credenciales
@@ -28,51 +28,22 @@ class AbmNaturalLanguage {
 
             $entitiesResponse = $this->languageClient->analyzeEntities($texto);
             $entidades = $entitiesResponse->entities();
-           
+        
             $syntaxResponse = $this->languageClient->analyzeSyntax($texto);
             $syntaxis = $syntaxResponse->tokens();
-            // print_r($syntaxis);
-            // Convertir los resultados en formato adecuado para la base de datos
-            $sentimientoScore = $sentimiento['score'];
-            $entidadesJson = $entidades[0]["name"]." ".$entidades[0]["name"]; 
-            $syntaxisJson = "sss";  
+            
+            if($entidades == [] ){
+$valEntidades = "";
+            }else{
+                $valEntidades = $entidades[0]["name"];
+            }
+            
             return [
                 'sentimiento' => $sentimiento['score'],
-                'entidades' =>  $entidades[0]["name"]." ".$entidades[0]["name"],
-                'syntaxis' => "Imperialismo"
+                'entidades' =>  $valEntidades,
+                'syntaxis' => "prueba"
             ];
         
     }
 
-    /**
-     * Listar las evaluaciones almacenadas en la base de datos.
-     */
-    public function listarEvaluaciones($parametro = "")
-    {
-        try {
-            $evaluaciones = Evaluacion::listar($parametro);
-            return $evaluaciones;
-        } catch (Exception $e) {
-            echo "Error al listar las evaluaciones: " . $e->getMessage();
-            return [];
-        }
-    }
-
-    /**
-     * Eliminar una evaluación de la base de datos
-     */
-    public function eliminarEvaluacion($id)
-    {
-        $evaluacion = new Evaluacion("", "", "", ""); // Crea un objeto Evaluacion vacío
-        return $evaluacion->eliminar($id); // Elimina la evaluación por ID
-    }
-
-    /**
-     * Modificar una evaluación en la base de datos
-     */
-    public function modificarEvaluacion($id, $nuevoSentimiento, $nuevasEntidades, $nuevaSintaxis, $nuevoTexto)
-    {
-        $evaluacion = new Evaluacion($nuevoSentimiento, $nuevasEntidades, $nuevaSintaxis, $nuevoTexto);
-        return $evaluacion->modificar($id); // Modifica la evaluación por ID
-    }
 }
