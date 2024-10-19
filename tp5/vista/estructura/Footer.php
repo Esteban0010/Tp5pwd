d9745689c4c8a781d5f454abcb03abb07aba899c
Esteban0010 <estebanpilchuman02@gmail.com>
2024-10-19 00:08:37 -0300

    prueba

--- tp5/vista/estructura/Footer.php
+++ tp5/vista/estructura/Footer.php
@@ -1,7 +1,7 @@
 </main>
 <footer class="bg-black" style="width: 100%; text-align: center; padding: 10px 0; color:white">
     <div>
-        Universidad Nacional del Comahue
+        Universidad Nacional del Comahue.
     </div>
 </footer>
 </body>
